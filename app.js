8ca57838da806291fcccc8f1fe470242e65fce8e
sunguide@qq.com <sunguide1989>
2017-11-27 17:27:05 +0800

    fix eslint

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
 'use strict';
-const kue = require("kue");
 const loadJob = require('./lib/load_job');
-const assert = require('assert');
+// const assert = require('assert');
 const qs = require('querystring');
 const path = require('path');
-const Job = require("./lib/job");
+const Job = require('./lib/job');
 module.exports = app => {
-    const jobs = loadJob(app);
+  const jobs = loadJob(app);
     // app.addSingleton('job', createJob);
     // let jobQueue = createJob(app.config.job.client, app);
-    app.job = new Job(app);
+  app.job = new Job(app);
     // for test purpose
-    app.runJob = jobPath => {
-      if (!path.isAbsolute(jobPath)) {
-          jobPath = path.join(app.config.baseDir, 'app/job', jobPath);
-      }
-      jobPath = require.resolve(jobPath);
-      let job;
+  app.runJob = jobPath => {
+    if (!path.isAbsolute(jobPath)) {
+      jobPath = path.join(app.config.baseDir, 'app/job', jobPath);
+    }
+    jobPath = require.resolve(jobPath);
+    let job;
 
-      try {
-          job = jobs[jobPath];
-          if (!job) {
-              throw new Error(`Cannot find job ${jobPath}`);
-          }
-      } catch (err) {
-          err.message = `[egg-job] ${err.message}`;
-          return Promise.reject(err);
+    try {
+      job = jobs[jobPath];
+      if (!job) {
+        throw new Error(`Cannot find job ${jobPath}`);
       }
+    } catch (err) {
+      err.message = `[egg-job] ${err.message}`;
+      return Promise.reject(err);
+    }
 
       // run with anonymous context
-      const ctx = app.createAnonymousContext({
-          method: 'JOB',
-          url: `/__job?path=${jobPath}&${qs.stringify(job.job)}`,
-      });
+    const ctx = app.createAnonymousContext({
+      method: 'JOB',
+      url: `/__job?path=${jobPath}&${qs.stringify(job.job)}`,
+    });
 
-      return job.task(ctx);
-    }
+    return job.task(ctx);
+  };
     // log job list
-    for (const s in jobs) {
-        const job = jobs[s];
-        if (!job.job.disable) app.coreLogger.info('[egg-job]: register job %s', job.key);
-    }
+  for (const s in jobs) {
+    const job = jobs[s];
+    if (!job.job.disable) app.coreLogger.info('[egg-job]: register job %s', job.key);
+  }
 
     // register job event
-    app.messenger.on('egg-job', data => {
-        app.coreLogger.info('[egg-job]: get message: %j', data);
-        const key = data.key;
-        const job = jobs[key];
+  app.messenger.on('egg-job', data => {
+    app.coreLogger.info('[egg-job]: get message: %j', data);
+    const key = data.key;
+    const job = jobs[key];
 
-        if (!job) {
-            app.coreLogger.warn(`[egg-job] unknown task: ${key}`);
-            return;
-        }
+    if (!job) {
+      app.coreLogger.warn(`[egg-job] unknown task: ${key}`);
+      return;
+    }
         /* istanbul ignore next */
-        if (job.job.disable) return;
+    if (job.job.disable) return;
 
         // run with anonymous context
-        const ctx = app.createAnonymousContext({
-            method: 'JOB',
-            url: `/__job?path=${key}&${qs.stringify(job.job)}`,
-        });
+    const ctx = app.createAnonymousContext({
+      method: 'JOB',
+      url: `/__job?path=${key}&${qs.stringify(job.job)}`,
+    });
 
-        const start = Date.now();
-        const task = job.task;
-        task(ctx)
+    const start = Date.now();
+    const task = job.task;
+    task(ctx)
             .then(() => true) // succeed
             .catch(err => {
-                err.message = `[egg-job] ${key} excute error. ${err.message}`;
-                app.logger.error(err);
-                return false; // failed
+              err.message = `[egg-job] ${key} excute error. ${err.message}`;
+              app.logger.error(err);
+              return false; // failed
             })
             .then(success => {
-                const rt = Date.now() - start;
-                const status = success ? 'succeed' : 'failed';
-                ctx.coreLogger.info(`[egg-job] ${key} excute ${status}, used ${rt}ms`);
+              const rt = Date.now() - start;
+              const status = success ? 'succeed' : 'failed';
+              ctx.coreLogger.info(`[egg-job] ${key} excute ${status}, used ${rt}ms`);
             });
-    });
+  });
 
-    app.beforeStart(function*() {
-        app.coreLogger.info(`[egg-job] instance status OK`);
-    });
+  app.beforeStart(function* () {
+    app.coreLogger.info('[egg-job] instance status OK');
+  });
 
 };
--- test/job.test.js
+++ test/job.test.js
@@ -3,22 +3,22 @@
 const request = require('supertest');
 const mm = require('egg-mock');
 
-describe('test/job.test.js', () => {
-  let app;
-  before(() => {
-    app = mm.app({
-      baseDir: 'apps/job-test',
-    });
-    return app.ready();
-  });
-
-  after(() => app.close());
-  afterEach(mm.restore);
-
+describe('test/job.test.js', async () => {
+  const app = mm.cluster({ baseDir: 'job-test', workers: 2, cache: false });
+  // app.debug();
+  await app.ready();
+  await sleep(2000);
   it('should GET /', () => {
+
     return request(app.callback())
       .get('/')
       .expect('hi, job')
       .expect(200);
   });
 });
+
+function sleep(time) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+}
